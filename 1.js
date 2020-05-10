8b0d75ad45d23d9155e348e5ae999f64f5215e80
Krutika <krutikaambarte23@gmail.com>
2020-05-10 19:11:59 +0530

    java program

---
+++ 1.js
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Java1</title>
+        <script>
+            document.write("hello");
+            </script>
+     </head>
+     <body>
+         <p>This is an example</p>
+
+
+     </body>       
+</html>
