455de9dc9ab8136e0a5354dd3eeb01a49ae77114
Krutika <krutikaambarte23@gmail.com>
2020-05-24 19:51:24 +0530

    javascript

--- 1.js
+++ 1.js
@@ -1,13 +1,1 @@
-<html>
-    <head>
-        <title>Java1</title>
-        <script>
-            document.write("hello");
-            </script>
-     </head>
-     <body>
-         <p>This is an example</p>
-
-
-     </body>       
-</html>
+alert ("Hello");
---
+++ 1.php
@@ -0,0 +1,10 @@
+<?php
+function print_me()
+{
+    echo "print_me";
+
+}
+echo "strat of program";
+print_my();
+echo "rest of program";
+?>
